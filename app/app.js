804727f59c70024dd0dc6c82550f6479c7f0ffdb
Andrew Kahn <andrew@andrewkahn.me>
2017-08-02 04:27:27 -0400

    fixes

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
+  // same dependency injection, different style
   // 'ngRoute',
   // 'myApp.view1',
   // 'myApp.view2'
@@ -11,7 +12,14 @@ angular.module('myApp', [
 
   // $routeProvider.otherwise({redirectTo: '/view2'});
 // }])
+run(function ($rootScope, $log, $location) {
+    $location.path('/');//Add this
+    // continue code here
+    $log.log('first log to console');
+}).
 controller('ctrler', ['$scope', '$log', function($scope, $log) {
+  $log.log('second text to the console');
+
   $scope.method = function() {
     $log.log('some text to the console');
   };
